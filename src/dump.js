94a4c1751a120a623f2d7704c0b437c1c4f7c54a
Martin <mr.mmaurer+work@gmail.com>
2019-03-23 11:39:43 +0100

    Added dump stuff

---
+++ src/dump.js
@@ -0,0 +1,43 @@
+import rp from 'request-promise-native';
+import fs from 'fs-extra';
+
+const query = url => {
+  return rp(url)
+    .then((content) => {
+      return content;
+    })
+    .catch((error) => {
+      throw new Error('error');
+    });
+}
+
+const dump = content => new Promise((resolve, reject) => {
+  const newFile = fs.createWriteStream(`${__dirname}/../dump/dump.html`);
+
+  newFile.on('error', () => {
+    reject();
+  });
+
+  newFile.on('finish', () => {
+    resolve('done');
+  });
+
+  newFile.write(content, 'utf8');
+  newFile.end();
+});
+
+query('https://www.instagram.com/rexiecat/?hl=en')
+  .then((content) => {
+    dump(content)
+      .then(() => {
+        console.error('Finished');
+
+        process.exit(0);
+      })
+      .catch((error) => {
+        process.exit(1);
+      });
+  })
+  .catch(() => {
+    process.exit(1);
+  });
--- src/query.js
+++ src/query.js
@@ -1,15 +1,18 @@
-import Instagram from 'node-instagram';
+import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const instagram = new Instagram({
+const instagram = {
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   accessToken: process.env.ACCESS_TOKEN,
-});
+};
 
-const getUserData = instagram.get('users/rexiecat');
+
+const url = 'https://api.instagram.com/v1/users/rexiecat/media/recent/?client_id=3bbd61a332384e66a46026c3dbbfaadc';
+
+const getUserData = fetch(url);
 
 getUserData.then((data) => {
   console.log(data);
