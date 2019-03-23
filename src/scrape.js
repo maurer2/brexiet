b145928260c54fa5c7a6f6bd9030ac6b3fa60159
Martin <mr.mmaurer+work@gmail.com>
2019-03-23 23:52:26 +0100

    Added html to json in scraper

--- src/scrape.js
+++ src/scrape.js
@@ -1,2 +1,52 @@
-import http from 'http';
+import rp from 'request-promise-native';
 import { JSDOM } from 'jsdom';
+
+const query = url => {
+  return rp(url)
+    .then((content) => {
+      return content;
+    })
+    .catch((error) => {
+      throw new Error('error');
+    });
+};
+
+const createDocument = htmlString => {
+  const document = new JSDOM(htmlString);
+
+  return document;
+};
+
+const findScriptTag = virtualDocument => {
+  const tags = Array.from(virtualDocument.window.document.querySelectorAll('script'));
+  const searchString = 'profile_pic_url';
+
+  const tagWithImages = tags.find((element) => {
+    const elementAsString = element.innerHTML;
+
+    return elementAsString.includes(searchString);
+  });
+
+  return tagWithImages;
+};
+
+query('https://www.instagram.com/rexiecat/?hl=en')
+  .then((content) => {
+    const virtualDocument = createDocument(content);
+    const scriptTag = findScriptTag(virtualDocument);
+
+    const scriptTagContent = scriptTag.innerHTML;
+
+    // remove invalid semicolon and leading string
+    let validScriptTagContent = scriptTagContent.replace('window._sharedData = ', '');
+        validScriptTagContent = validScriptTagContent.slice(0, -1);
+
+    const parsedScritpTag = JSON.parse(validScriptTagContent);
+
+    console.log(parsedScritpTag);
+
+    process.exit(0);
+  })
+  .catch(() => {
+    process.exit(1);
+  });
