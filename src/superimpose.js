ad90874c4b3f40d1077aeb28be6f940bbabff8f0
Martin <mr.mmaurer+work@gmail.com>
2019-03-24 22:42:43 +0100

    Removed console output

--- src/superimpose.js
+++ src/superimpose.js
@@ -1,23 +1,33 @@
 import { createCanvas, loadImage } from 'canvas';
-import terminalImage from 'terminal-image';
+// import terminalImage from 'terminal-image';
+import fs from 'fs-extra';
 
 const canvas = createCanvas(640, 360);
 const context = canvas.getContext('2d');
 
-// font styles
-context.font = '148px "Arial"';
-context.fillStyle = 'red';
-context.fillText('Rexie Cat', 320, 180);
+// output
+const newFile = fs.createWriteStream('./dist/test.jpeg');
 
 loadImage('./dump/image.jpg')
   .then((background) => {
     context.drawImage(background, 0, 0, 640, 480);
 
+    // font stuff
+    context.font = '48px "Arial"';
+    context.fillStyle = 'black';
+    // context.textAlign = 'center';
+    context.fillText('Rexie Cat', 50, 50);
+
     const canvasAsBuffer = canvas.toBuffer();
+
+    newFile.write(canvasAsBuffer);
+
+    /*
     const consoleImage = terminalImage.buffer(canvasAsBuffer);
 
     consoleImage
       .then((image) => {
         console.log(image);
       });
+    */
   });
